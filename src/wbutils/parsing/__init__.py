9662c766e975fef5ba5200b4ff35055ece916ec5
ankur-gupta <7110058+ankur-gupta@users.noreply.github.com>
2023-07-18 20:07:11 -0700

    Fixing imports

--- src/wbutils/parsing/__init__.py
+++
@@ -1,8 +0,0 @@
-from wbutils.framework import set_framework, get_framework, PYTORCH_FRAMEWORK, TENSORFLOW_FRAMEWORK, DEFAULT_FRAMEWORK
-
-FRAMEWORK = get_framework()
-
-if FRAMEWORK == PYTORCH_FRAMEWORK:
-    from ._pytorch import CONFIG_ITEMS_FROM_STR, parse_wb_config, get_optimizer_cls, get_loss_cls
-elif FRAMEWORK == TENSORFLOW_FRAMEWORK:
-    raise NotImplementedError(f'Framework {FRAMEWORK} is not yet implemented')
